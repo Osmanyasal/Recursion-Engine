416a9c002c197cd9cf93ebe5a85a6f5f32624560
rt7 <oyasal@gmail.com>
2023-07-13 16:58:29 +0300

    event binder implementation

--- src/core/events/event.hh
+++ src/core/events/event.hh
@@ -5,7 +5,7 @@
 #include <sstream>
 
 // put your event reference, it returns the operator<< string to you regarding class hierarchy.
-#define EVENT_LOG(expression)   \
+#define EVENT_TO_STRING(expression)   \
     [&]() -> std::string {      \
         std::ostringstream oss; \
         oss << expression;      \
@@ -48,6 +48,7 @@ namespace Recursion::core::events
     };
 
 #define SET_EVENT_TYPE(type)                                           \
+    static EventType get_static_event_type() { return type; }          \
     virtual EventType get_event_type() const override { return type; } \
     virtual std::string get_name() const override { return #type; }
 
@@ -79,6 +80,36 @@ namespace Recursion::core::events
             << "is_handled=" << event.is_handled << ", ";
         return out;
     }
+
+    /**
+     * @brief Binds event to a function that takes an event parameter T and returns boolean
+     * 
+    */
+    class EventBinder
+    {
+        template <typename T>
+        using EventFunc = std::function<bool(T &)>;
+
+    public:
+        EventBinder(Event &event)
+            : m_event{event}
+        {
+        }
+
+        template <typename T>
+        bool bind(EventFunc<T> func)
+        {
+            if (m_event.get_event_type() == T::get_static_event_type())
+            {
+                m_event.is_handled = func(*(T *)&m_event);
+                return true;
+            }
+            return false;
+        }
+
+    private:
+        Event &m_event;
+    };
 
 } // namespace Recursion::core::events
 
--- src/core/main.cc
+++ src/core/main.cc
@@ -4,9 +4,18 @@
 
 using namespace Recursion::core::events;
 
+bool handle_key_press(Event& event){
+    REC_CORE_INFO("handle_key_press is executed");
+    REC_CORE_INFO("event is {}",EVENT_TO_STRING((KeyPressEvent&)event));
+    return true;
+}
 int main(int argc, char **argv)
 {
     Proj1 proj;
+    KeyPressEvent keypress{REC_KEY_A};
+
+    EventDispatcher dispatcher{keypress};
+    dispatcher.dispatch<KeyPressEvent>(&handle_key_press);    
     proj.start();
     return 0;
 }
